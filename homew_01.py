1c9d616ff258286d98bed736bd587ff4cbcab2e5
Mariia Leonova <massha.leonova@yandex.ru>
2023-10-16 22:54:55 +0300

    edits made

--- homew_01.py
+++ homew_01.py
@@ -1,51 +1,55 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grade = {}
 
     def rate_lector(self, teacher, course, grade):
-        if isinstance(teacher, Lecturer) and course in self.finished_courses and course in courses_attached and grade in range(1, 10):
-            # if course in teacher.grades:
-            teacher.grades.setdefault(course, [grade])
-            #     teacher.grades[course] += [grade]
-            # else:
-            #     teacher.grades[course] = [grade]
+        if isinstance(teacher, Lecturer) and course in self.finished_courses and course in teacher.courses_attached and grade in range(1, 10):
+            if course in teacher.grade:
+                teacher.grade[course] += [grade]
+            else:
+                teacher.grade[course] = [grade]
+                # я только не пойму, почему не срабатывает, если заменить эти 4 стоки на teacher.grades.setdefault(course, [grade])
         else:
             return 'Ошибка'
 
     def __str__(self):
-        res = f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания:{self.grade.values()} \nКурсы в процессе изучения:{self.courses_in_progress} \nЗавершенные курсы:{self.finished_courses}"
+        res = f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: {self.average} \nКурсы в процессе изучения: {', '.join(self.courses_in_progress)} \nЗавершенные курсы: {', '.join(self.finished_courses)}"
         return res
 
     def mean(self):
-        grades_from_lecturers = []
+        grade_from_lecturers = []
         for numb in self.grade.values():
             for n in numb:
-                grades_from_lecturers.append(n)
-        self.average = sum(grades_from_lecturers) / len(grades_from_lecturers)
+                grade_from_lecturers.append(n)
+        self.average = sum(grade_from_lecturers) / len(grade_from_lecturers)
 
-    def compare(self, other_students):
-        if isinstance(other_students, Student):
-            if other_students.average < self.average:
-                self.status = "самый умный студент"
-            else:
-                self.status = "не самый умный студент..."
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student!')
+            return
+        return self.average < other.average
+
+    def __gt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student!')
+            return
+        return self.average > other.average
 
 class Mentor:
-    def __init__(self, name, surname, grade = {}):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.grade = grade
+        self.grade = {}
         self.courses_attached = []
 
 
 class Lecturer(Mentor):
-    def __init__(self, name, surname, grade = {}):
-        self.grade = grade
+    def __init__(self, name, surname):
+        self.grade = {}
         super().__init__(name, surname)
 
     def mean(self):
@@ -54,26 +58,31 @@ class Lecturer(Mentor):
             for n in numb:
                 grades_student.append(n)
         self.average = sum(grades_student) / len(grades_student)
-        # не понимаю, почему если просто вызвать print(cool_mentor.average), то все ок, а если засунуть этот average в str, то он не видит этот атрибут. Дело в отступе?..
 
     def __str__(self):
-        res = f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции:{self.average}"
+        res = f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.average}"
         return res
 
-    def compare(self, other_lecturers):
-        if isinstance(other_lecturers, Lecturer):
-            if other_lecturers.average < self.average:
-                self.status = "самый умный препод"
-            else:
-                self.status = "не самый умный препод..."
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer!')
+            return
+        return self.average < other.average
+
+    def __gt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer!')
+            return
+        return self.average > other.average
+        # самостоятельно метод gt после переписки lt, как говорилось в лекции, не переписался
 
 class Reviewer(Mentor):
     def review(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
+            if course in student.grade:
+                student.grade[course] += [grade]
             else:
-                student.grades[course] = [grade]
+                student.grade[course] = [grade]
         else:
             return 'Ошибка'
 
@@ -81,33 +90,72 @@ class Reviewer(Mentor):
         res = f"Имя: {self.name} \nФамилия: {self.surname}"
         return res
 
-# some_rev = Reviewer("ff", "dd")
-#
-# print(some_rev)
 
-best_student = Student('Ruoy', 'Eman', 'boy')
-best_student.courses_in_progress += ['Python']
 
-cool_mentor = Lecturer('Some', 'Buddy')
+student_one = Student('Ruoy', 'Eman')
+student_one.courses_in_progress += ['Python']
+student_one.finished_courses += ['Python']
+student_one.finished_courses += ['Git']
+student_two = Student('Emma', 'Jones')
+student_two.courses_in_progress += ['Python']
+student_two.finished_courses += ['Python']
+
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
-cool_mentor.grade.setdefault('python', [])
-cool_mentor.grade.setdefault('python', [3])
-cool_mentor.grade['python'].append(3)
-cool_mentor.grade['python'].append(4)
-cool_mentor.grade['python'].append(2)
-# cool_mentor.mean(cool_mentor.grade)
+cool_mentor_two = Lecturer('James', 'Last')
+cool_mentor_two.courses_attached += ['Python']
+cool_mentor_three = Lecturer('Michael', 'Jackson')
+cool_mentor_three.courses_attached += ['Python']
 
-# print(cool_mentor.grade)
-print(cool_mentor)
-print(cool_mentor.mean)
-# best_student.finished_courses.append('a')
-# print(best_student.finished_courses)
-# print(best_student)
-cool_mentor.mean()
-print(cool_mentor.average)
-# cool_mentor.mean(cool_mentor.grade)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-#
-# print(best_student.grades)
+cool_mentor.review(student_one, 'Python', 2)
+cool_mentor.review(student_one, 'Python', 3)
+cool_mentor.review(student_one, 'Python', 7)
+cool_mentor.review(student_two, 'Python', 3)
+cool_mentor.review(student_two, 'Python', 4)
+cool_mentor.review(student_two, 'Python', 8)
+student_one.mean()
+student_two.mean()
+# print(student_one.grade)
+# print(student_two.grade)
+# print(student_one.average)
+# print(student_two.average)
+# print(student_one)
+# print(cool_mentor.courses_attached)
+student_one.rate_lector(cool_mentor_two, 'Python', 4)
+student_one.rate_lector(cool_mentor_two, 'Python', 4)
+student_two.rate_lector(cool_mentor_two, 'Python', 1)
+student_one.rate_lector(cool_mentor_three, 'Python', 8)
+student_two.rate_lector(cool_mentor_three, 'Python', 1)
+# print(cool_mentor_two.grade)
+cool_mentor_two.mean()
+cool_mentor_three.mean()
+
+
+def average_student_mean(course, students):
+    all_means = []
+    for student in students:
+        all_means.append(student.average)
+    average_means = sum(all_means) / len(all_means)
+    return average_means
+
+def average_lecturer_mean(course, lecturers):
+    all_means = []
+    for lecturer in lecturers:
+        all_means.append(lecturer.average)
+    average_means = sum(all_means) / len(all_means)
+    return average_means
+
+print(average_student_mean('Python', [student_one, student_two]))
+print(average_lecturer_mean('Python', [cool_mentor_two, cool_mentor_three]))
+
+# print(cool_mentor_two.average)
+# print(cool_mentor_three.average)
+# print(cool_mentor)
+# print(cool_mentor_two)
+
+# print(cool_mentor_two.__lt__(cool_mentor_three))
+# print(cool_mentor_two.__gt__(cool_mentor_three))
+# print(student_one.__lt__(student_two))
+# print(student_one.__gt__(student_two))
+
+
